69e092096e985f22a8d9772dd479c2427159a3f8
yardwerkz <123331694+yardwerkz@users.noreply.github.com>
2025-01-26 17:24:10 -0700

    Got UIx to work

--- src/client/balthasar/main.cljs
+++ src/client/balthasar/main.cljs
@@ -1,19 +1,15 @@
 (ns balthasar.main
-  (:require
-   [uix.core :as u :refer [defui $]]
-   [uix.dom]))
+  (:require [uix.core :as uix :refer [defui $]]
+            [uix.dom]))
 
-(defn ^:dev/after-load start []
-  (js/console.log "Start"))
+(defui app []
+  ($ :h1 "Hello, Balthasar!"))
 
-(defn init! []
-  (js/console.log "Init")
-  (let [root-element (js/document.getElementById "app")]
-    (uix.dom/render root-element
-                    [:div
-                     [:h1 "Hello, Balthasar!"]
-                     [:p "This is a ClojureScript SPA."]]))
-  (start))
+(defonce root
+  (uix.dom/create-root (js/document.getElementById "root")))
 
-(defn ^:dev/before-load stop []
-  (js/console.log "Stop"))
+(defn render []
+  (uix.dom/render-root ($ app) root))
+
+(defn ^:export init! []
+  (render))
